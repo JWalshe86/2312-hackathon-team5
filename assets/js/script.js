e45a3a0c71ff99194dcc23c4eed1973312805a92
John Walshe <walshejohnnyw7@gmail.com>
2023-12-14 18:02:30 +0000

    random gift for username display

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,62 @@ const GenerateGift = function (){
 
 let giftGenerated = GenerateGift();
 
-console.log(giftGenerated);
+// Name Class: Represents a Name
+class Name {
+    constructor(title){
+        this.title = title;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+    static displayNames() {
+    
+        names.forEach(name=> UI.addNameToList(name));
+
+    }
+
+    static addNameToList(name){
+        const list = document.querySelector('#name-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${name.title}</td>
+            <td>${giftGenerated}</td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static clearFields() {
+        document.querySelector('#nameInput').value = '';
+    }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Names
+document.addEventListener('DOMContentLoaded', UI.displayNames);
+
+// Event: Add a Name
+document.querySelector('#name-form').addEventListener('submit',
+(e)=> {
+// Prevent actual submit 
+e.preventDefault();
+
+// Get form values
+const title = document.querySelector('#nameInput').value;
+
+// Instatiate name
+const name = new Name(title);
+
+// Add name to UI
+UI.addNameToList(name);
+
+// Clear fields
+UI.clearFields()
+
+});
+
