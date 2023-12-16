8364f3512c11d40ddd0f112f6fb2585b19f57395
John Walshe <walshejohnnyw7@gmail.com>
2023-12-16 20:03:37 +0000

    add cursor styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -197,3 +197,18 @@ $(document).snowfall({
   minSpeed: 1,     // min speed of snowflake, 1 by default
   maxSpeed: 3      // max speed of snowflake, 5 by default
 });
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
